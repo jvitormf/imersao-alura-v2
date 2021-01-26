fcd76039dfda14660cb4cda0db1da91e1b10a5b9
João Vitor <j.vitor.mf@gmail.com>
2021-01-26 15:08:50 -0300

    chore: update meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
@@ -23,17 +25,42 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Quiz Javascript</title>
+        <meta name="title" content="Quiz Javascript" />
+        <meta name="description" content="" />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://imersao-alura-v2.jvitormf.vercel.app/" />
+        <meta property="og:title" content="Quiz Javascript" />
+        <meta property="og:description" content="Quiz Javascript - Imersão React - Next.js | Alura" />
+        <meta property="og:image" content="https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://imersao-alura-v2.jvitormf.vercel.app/" />
+        <meta property="twitter:title" content="Quiz Javascript" />
+        <meta property="twitter:description" content="Quiz Javascript - Imersão React - Next.js | Alura" />
+        <meta property="twitter:image" content="https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
