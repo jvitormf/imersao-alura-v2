2e49e7a7b86c427728d98ced96cba7e6b930a2d8
João Vitor <j.vitor.mf@gmail.com>
2021-01-25 23:12:18 -0300

    chore: add meta tags

--- pages/index.js
+++ pages/index.js
@@ -26,23 +26,30 @@ export const QuizContainer = styled.div`
 export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <head>
+        <meta property="og:title" content="Quiz Javascript"/>
+        <meta property="og:description" content="Quiz Javascript - Imersão React - Next | Alura"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://imersao-alura-v2.jvitormf.vercel.app/"/>
+        <meta property="og:image" content="https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80"/>
+      </head>
       <QuizContainer>
 
         <Widget>
           <Widget.Header>
-                <h1>The legend of zelda</h1>
+                <h1>Javascript</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>lorem ipsum dolor sit amet...</p>
+            <p>Lorem ipsum dolor sit amet</p>
           </Widget.Content>
         </Widget>
 
         <Widget>
           <Widget.Header>
-                <h1>Quizes da galera</h1>
+                <h1>Quizes da Galera</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>lorem ipsum dolor sit amet...</p>
+            <p>Lorem ipsum dolor sit amet</p>
           </Widget.Content>
         </Widget>
 
