6bd53170f9924d4cada165ab33e1d8a7004dfabc
João Vitor <j.vitor.mf@gmail.com>
2021-01-29 21:13:50 -0300

    feat: add quiz screen for other quizzes

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
-export default function QuizdaGaleraPage() {
+import QuizScreen from '../../src/screens/Quiz';
+
+export default function QuizdaGaleraPage({ externalDb }) {
   return (
-    <div>
-      desafio
-    </div>
+    <ThemeProvider theme={externalDb.theme}>
+      <QuizScreen externalQuestions={externalDb.questions} externalBg={externalDb.bg} />
+    </ThemeProvider>
   );
+}
+
+export async function getServerSideProps(context) {
+  const [projectName, githubUser] = context.query.id.split('___');
+  const externalDb = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
+    .then((serverResponse) => {
+      if (serverResponse.ok) {
+        return serverResponse.json();
+      }
+      throw new Error('Falha ao acessar database');
+    })
+    .then((serverResponseConvertedToObject) => serverResponseConvertedToObject)
+    .catch((err) => {
+      throw new Error('Erro ao acessar api.');
+    });
+
+  return {
+    props: {
+      externalDb,
+    },
+  };
 }
